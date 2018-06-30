69c291e65ea48d8c2012af701de851f30f969e74
mrome007 <mrome007@ucr.edu>
2018-06-30 16:11:11 -0700

    Start of voxels project

---
+++ Assets/WorldController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour 
+{
+    public GameObject Block;
+    public int WorldSize = 2;
+
+    public IEnumerator BuildWorld()
+    {
+        for(int z = 0; z < WorldSize; z++)
+        {
+            for(int y = 0; y < WorldSize; y++)
+            {
+                for(int x = 0; x < WorldSize; x++)
+                {
+                    var pos = new Vector3(x, y, z);
+                    var cube = GameObject.Instantiate(Block, pos, Quaternion.identity);
+                    cube.name = x + "_" + y + "_" + z;
+                }
+                yield return null;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(BuildWorld());
+    }
+}
